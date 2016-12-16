d8891a092b7bbfe790c29ff92fcd797d77db702d
Mohammed Fadin <mfadin@sa.ibm.com>
2016-12-16 15:54:20 +0300

    Uploading base code

---
+++ app.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+const PORT = 3002 | process.env.PORT;
+
+function handleRequest (req, res){
+	res.end("It works! \nURL: " + req.url)
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function () {
+	console.log("Started \n Port: " + PORT)
+})
